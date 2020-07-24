6358c38de8911091e2b0242a173b5b8016852205
Hani Gerges <hani@localhost.localdomain>
2020-07-25 00:43:30 +0300

    contact icons fixed by secure_asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,9 +18,9 @@
 
   <!-- Custom fonts for this template -->
   <link href="{{secure_asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+  <link href='{{secure_asset('https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic')}}' rel='stylesheet' type='text/css'>
+  <link href='{{secure_asset('https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800')}}' rel='stylesheet' type='text/css'>
+  <link href="{{secure_asset('https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css')}}" rel="stylesheet">
 
   <!-- Custom styles for this template -->
   <link href="{{secure_asset('css/clean-blog.min.css')}}" rel="stylesheet">
